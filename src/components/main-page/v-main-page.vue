5c4752fccc3403da92edf2b5abf7dd766779fbbc
Ismail Abazov <41200208+ismabazoff@users.noreply.github.com>
2020-06-30 21:13:45 +0300

    Add files via upload

---
+++ src/components/main-page/v-main-page.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="v-main-page">
+    <h1>Интернет-магазин</h1>
+    <p>
+      <router-link :to="{name: 'catalog'}">Каталог</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-main-page",
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {}
+};
+</script>
+
+<style scoped>
+</style>
